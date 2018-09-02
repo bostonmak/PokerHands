974dea6c7812ad5576eea4e92325596097fe9ce1
BOSTON-GAMING\Boston <boston@makotexas.com>
2018-09-02 14:25:25 -0700

    calculate hands, begin hand comparison

--- src/Hand.java
+++ src/Hand.java
@@ -1,8 +1,11 @@
 
+import java.util.List;
+import java.util.ArrayList;
 
 public class Hand {
     public String fiveValue, quadValue, tripValue, pair1Value, pair2Value, highCardValue;
     public HandTypes type;
+    public List<String> hand;
 
     public Hand() {
         fiveValue = "";
@@ -12,5 +15,6 @@ public class Hand {
         pair2Value = "";
         highCardValue = "";
         type = HandTypes.HIGH_CARD;
+        hand = new ArrayList<>();
     }
 }
--- src/PokerHands.java
+++ src/PokerHands.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeSet;
 import javafx.util.Pair;
 
 
@@ -17,6 +20,8 @@ public class PokerHands {
                 Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"));
     private List<String> validSuits = new ArrayList<>(Arrays.asList("D", "C", "H", "S"));
 
+    private final int HAND_SIZE = 5;
+
     public static PokerHands getInstance() {
         if (instance == null) {
             instance = new PokerHands();
@@ -24,19 +29,41 @@ public class PokerHands {
         return instance;
     }
 
-    public void init() {
+    public void run() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to PokerHands by Boston Mak.");
         System.out.println("Cards should be formatted as <Value><Suit>, such as 'AS' for Ace of Spades or '9H' for 9 of Hearts.");
         System.out.println("Hands should be formatted as a comma-separated list of cards, such as 'AS, 9H, KS, 4D, 5C'.");
 
+        Hand hand1 = new Hand();
+        Hand hand2 = new Hand();
+
+        // Keep polling until proper hands are provided
+        while(!poll(scan, hand1, hand2));
+
+        calculateHand(hand1);
+        calculateHand(hand2);
+
+        System.out.println(compareHands(hand1, hand2));
+
+        System.out.println("Press enter to exit: ");
+        scan.nextLine();
+    }
+
+    public boolean poll(Scanner scan, Hand p1Hand, Hand p2Hand) {
         boolean success = false;
         // Keep requesting a hand until one is valid
         while (!success) {
             System.out.println("Please enter the hand for Player 1:");
             String hand = scan.nextLine();
-            p1Hand = Arrays.asList(hand.split(","));
+            p1Hand.hand = Arrays.asList(hand.split(","));
             Pair<Boolean, String> result = checkHand(p1Hand);
+            success = result.getKey();
+            if (!success) {
+                System.out.println(result.getValue());
+                continue;
+            }
+            result = checkDupesInHand(p1Hand);
             success = result.getKey();
             System.out.println(result.getValue());
         }
@@ -45,21 +72,34 @@ public class PokerHands {
         while (!success) {
             System.out.println("Please enter the hand for Player 2:");
             String hand = scan.nextLine();
-            p2Hand = Arrays.asList(hand.split(","));
+            p2Hand.hand = Arrays.asList(hand.split(","));
             Pair<Boolean, String> result = checkHand(p2Hand);
+            success = result.getKey();
+            if (!success) {
+                System.out.println(result.getValue());
+                continue;
+            }
+            result = checkDupesInHand(p2Hand);
             success = result.getKey();
             System.out.println(result.getValue());
         }
+
+        Pair<Boolean, String> dupes = checkDupes(p1Hand, p2Hand);
+        if (!checkDupes(p1Hand, p2Hand).getKey()) {
+            System.out.println(dupes.getValue());
+            return false;
+        }
+        return true;
     }
 
-    public Pair<Boolean, String> checkHand(List<String> hand) {
-        if (hand.size() != 5) {
-            return new Pair<>(false, String.format("Invalid hand size of %d\n", hand.size()));
+    public Pair<Boolean, String> checkHand(Hand hand) {
+        if (hand.hand.size() != HAND_SIZE) {
+            return new Pair<>(false, String.format("Invalid hand size of %d\n", hand.hand.size()));
         }
-        for (int i = 0; i < hand.size(); ++i) {
-            String card = hand.get(i).trim().toUpperCase();
+        for (int i = 0; i < hand.hand.size(); ++i) {
+            String card = hand.hand.get(i).trim().toUpperCase();
             Pair<Boolean, String> result = checkCard(card);
-            hand.set(i, card);
+            hand.hand.set(i, card);
             if (!result.getKey()) {
                 return result;
             }
@@ -76,32 +116,49 @@ public class PokerHands {
         return new Pair<>(false, String.format("Invalid card %s\n", card));
     }
 
-    public Hand calculateHand(List<String> hand) {
+    public Pair<Boolean, String> checkDupes(Hand hand1, Hand hand2) {
+        for (String card : hand1.hand) {
+            if (hand2.hand.indexOf(card) != -1) {
+                return new Pair<>(false, String.format("Duplicate card %s\n", card));
+            }
+        }
+        return new Pair<>(true, "");
+    }
+
+    public Pair<Boolean, String> checkDupesInHand(Hand hand) {
+        Set set = new HashSet(hand.hand);
+        if (set.size() != HAND_SIZE) {
+            return new Pair<>(false, "Duplicate cards in hand are not allowed");
+        }
+        return new Pair<>(true, "");
+    }
+
+    public void calculateHand(Hand hand) {
         Map values = new HashMap<String, Integer>();
-        Map suits = new HashMap<String, Integer>();
-        for (int i = 0; i < hand.size(); ++i) {
-            String card = hand.get(i);
+        String suit = "";
+        boolean suited = true;
+        for (int i = 0; i < hand.hand.size(); ++i) {
+            String card = hand.hand.get(i);
             String value = card.substring(0, card.length() - 1);
-            String suit = card.substring(card.length() - 1);
+            String cardSuit = card.substring(card.length() - 1);
             if (values.containsKey(value)) {
                 values.put(value, ((Integer)values.get(value)) + 1);
             }
             else {
                 values.put(value, 1);
             }
-            if (suits.containsKey(suit)) {
-                suits.put(suit, ((Integer)suits.get(suit)) + 1);
+            if (suit.equals("")) {
+                suit = cardSuit;
             }
-            else {
-                suits.put(suit, 1);
+            else if (!suit.equals(cardSuit)){
+                suited = false;
             }
         }
 
-        return calculateHandFromCards(values, suits);
+        calculateHandFromCards(hand, values, suited);
     }
 
-    public Hand calculateHandFromCards(Map<String, Integer> values, Map<String, Integer> suits) {
-        Hand hand = new Hand();
+    public Hand calculateHandFromCards(Hand hand, Map<String, Integer> values, boolean suited) {
         // Check matching values
         for (Map.Entry<String, Integer> entry : values.entrySet()) {
             // Check four of a kind
@@ -123,7 +180,10 @@ public class PokerHands {
             else if (entry.getValue() == 2) {
                 if (hand.pair1Value.equals("")) {
                     hand.pair1Value = entry.getKey();
-                    if (hand.type.ordinal() < HandTypes.ONE_PAIR.ordinal()) {
+                    if (!hand.tripValue.equals("")) {
+                        hand.type = HandTypes.FULL_HOUSE;
+                    }
+                    else if (hand.type.ordinal() < HandTypes.ONE_PAIR.ordinal()) {
                         hand.type = HandTypes.ONE_PAIR;
                     }
                 }
@@ -133,14 +193,96 @@ public class PokerHands {
                 }
             }
             // Check high card
-            else if (values.get(hand.highCardValue) < values.get(entry.getKey())) {
+            else if (validValues.indexOf(hand.highCardValue) < validValues.indexOf(entry.getKey())) {
                 hand.highCardValue = entry.getKey();
             }
         }
 
-        // Check hands based on suits
+        // Check straight
+        boolean isStraight = false;
+        if (values.keySet().size() == HAND_SIZE) {
+            isStraight = true;
+            TreeSet<String> keySet = new TreeSet<>(values.keySet());
+            int prevIndex = -1;
+            for (String value : keySet) {
+                if (prevIndex == -1 || prevIndex + 1 == validValues.indexOf(value)) {
+                    prevIndex = validValues.indexOf(value);
+                }
+                else {
+                    isStraight = false;
+                    break;
+                }
+            }
+            if (isStraight) {
+                hand.type = HandTypes.STRAIGHT;
+            }
+        }
 
+        // Check flush
+        if (suited && isStraight) {
+            hand.type = HandTypes.STRAIGHT_FLUSH;
+        }
+        else if (suited) {
+            hand.type = HandTypes.FLUSH;
+        }
 
-        return result;
+        return hand;
+    }
+
+    public String compareHands(Hand h1, Hand h2) {
+        if (h1.type.ordinal() > h2.type.ordinal()) {
+            String win = h1.type.name().replace("_", " ");
+            return String.format("Player 1 wins with a %s!\n", win);
+        }
+        else if (h1.type.ordinal() < h2.type.ordinal()) {
+            String win = h2.type.name().replace("_", " ");
+            return String.format("Player 2 wins with a %s!\n", win);
+        }
+        else {
+            if (h1.type == HandTypes.STRAIGHT_FLUSH) {
+                if (validValues.indexOf(h1.highCardValue) > validValues.indexOf(h2.highCardValue)) {
+                    String win = h1.type.name().replace("_", " ");
+                    return String.format("Player 1 wins with a %s-high %s!\n", h1.highCardValue, win);
+                }
+                else if (validValues.indexOf(h1.highCardValue) < validValues.indexOf(h2.highCardValue)) {
+                    String win = h2.type.name().replace("_", " ");
+                    return String.format("Player 2 wins with a %s-high %s!\n", h2.highCardValue, win);
+                }
+            }
+            else if (h1.type == HandTypes.FOUR_OF_A_KIND) {
+                if (validValues.indexOf(h1.quadValue) > validValues.indexOf(h2.quadValue)) {
+                    String win = h1.type.name().replace("_", " ");
+                    return String.format("Player 1 wins with a %s %s!\n", h1.quadValue, win);
+                }
+                else {
+                    String win = h2.type.name().replace("_", " ");
+                    return String.format("Player 2 wins with a %s %s!\n", h2.quadValue, win);
+                }
+            }
+            else if (h1.type == HandTypes.FULL_HOUSE) {
+                if (validValues.indexOf(h1.tripValue) > validValues.indexOf(h2.tripValue)) {
+                    String win = h1.type.name().replace("_", " ");
+                    return String.format("Player 1 wins with a stronger %s!\n", h1.tripValue, win);
+                }
+                else if (validValues.indexOf(h1.tripValue) < validValues.indexOf(h2.tripValue)) {
+                    String win = h2.type.name().replace("_", " ");
+                    return String.format("Player 2 wins with a stronger %s!\n", h2.tripValue, win);
+                }
+                else if (validValues.indexOf(h1.tripValue) == validValues.indexOf(h2.tripValue) &&
+                         validValues.indexOf(h1.pair1Value) > validValues.indexOf(h2.pair1Value)) {
+                    String win = h1.type.name().replace("_", " ");
+                    return String.format("Player 1 wins with a stronger %s!\n", h1.quadValue, win);
+                }
+                else if (validValues.indexOf(h1.tripValue) == validValues.indexOf(h2.tripValue) &&
+                        validValues.indexOf(h1.pair1Value) < validValues.indexOf(h2.pair1Value)) {
+                    String win = h2.type.name().replace("_", " ");
+                    return String.format("Player 2 wins with a stronger %s!\n", h2.quadValue, win);
+                }
+            }
+            else if (h1.type == HandTypes.FLUSH) {
+                return "";
+            }
+        }
+        return "";
     }
 }
